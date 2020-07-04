f01a6c0a89d25aa109fd1b3e980d632435c0c6d0
Aminat.A <aminatadesola@gmail.com>
2020-07-04 16:10:45 -0500

    planner html deleted and save btn working

--- script.js
+++ script.js
@@ -1,51 +1,80 @@
 // Current Day and Time
-$( document ).ready(function() {
-var date = moment().format("");
+$(document).ready(function () {
+  var date = moment().format("");
 
-$("#currentDay").append(date);
+  $("#currentDay").append(date);
 
 
-console.log(date);
+  console.log(date);
 
-// Time of day color change 
+  // Time of day color change 
 
-var timeofDay = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+  var timeofDay = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
-function updatetime() {
-  var currentTime = moment().format('H');
-  console.log( "currentTime:" + currentTime);
-    for(var i = 0; i < timeofDay.length; i++) {
-      
+  function updatetime() {
+    var currentTime = moment().format('H');
+    console.log("currentTime:" + currentTime);
+    for (var i = 0; i < timeofDay.length; i++) {
+
       if (parseInt(timeofDay[i]) > currentTime) {
         $("#" + timeofDay[i]).attr("style", "background-color: #58ce7b");
-        
-        
+
+
       }
       else if (parseInt(timeofDay[i]) < currentTime) {
         $("#" + timeofDay[i]).attr("style", "background-color: #ff9aa2");
-        
+
       }
       else if (parseInt(timeofDay[i]) == currentTime) {
         $("#" + timeofDay[i]).attr("style", "background-color: #f1f8ff");
-        
+
       }
     }
   }
   updatetime();
 
- 
- var savData = document.querySelectorAll(".btn").length;
+  //   function getInputValue(){
+  //     // Selecting the input element and get its value 
+  //     var inputVal = document.getElementsByClassName("time").value;
 
- console.log(savData);
+  //     // Displaying the value
+  //     console.log(inputVal);
+  // }
 
- for (var i =0; i < savData; i++){
-   document.querySelectorAll(".btn")[i].addEventListener("click", function() {
-    event.preventDefault()
-    var input = document.querySelectorAll(valueOf(".time"));
-    localStorage.setItem("time", input);
+  // getInputValue();
 
 
-  })
-}
+  $(".btn").on("click", function () {
+    var userInput = $(this).parent().parent().children(":nth-child(2)").val();
+    var timeSlot = $(this).parent().parent().children(":nth-child(1)").children(":nth-child(1)").text();
+    console.log(timeSlot)
+    console.log(userInput);
+
+    localStorage.setItem(timeSlot, userInput);
+
+
+
+  }
+  );
+
+
+  
+$("#9").val(localStorage.getItem("9 A.M."));
+$("#10").val(localStorage.getItem("10 A.M."));
+
+
+  //  var savData = document.querySelectorAll(value(".btn")).length;
+
+  //  console.log(savData);
+
+  //  for (var i =0; i < savData; i++){
+  //    document.querySelectorAll(".btn")[i].addEventListener("click", function() {
+  //     event.preventDefault()
+  //     var input = document.querySelectorAll(valueOf(".time"));
+  //     localStorage.setItem("time", input);
+
+
+  //   })
+  // }
 
 });
